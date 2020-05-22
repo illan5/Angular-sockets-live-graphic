7a6e072a0b782935258105311fec895d5aef06fb
Jose Illan <jose_illan@hotmail.com>
2020-05-22 19:59:18 +0200

    Start project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
+const config: SocketIoConfig = { url: 'http://localhost:5000', options: {} };
+ 
+import { ChartsModule } from 'ng2-charts';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +13,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SocketIoModule.forRoot(config),
+    ChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
